a9dc1ce475a0fbe2b767956b7927db492d3b312f
amakadiya98 <64594631+amakadiya98@users.noreply.github.com>
2024-08-21 17:02:23 +0530

    make a responsive web page

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     const nextIcon = '<img class="nextIcon" src="./assets/images/prewIcon.svg">'
     const prewIcon = '<img class="prewIcon" src="./assets/images/nextIcon.svg">'
-    $(".owl-carousel").owlCarousel({
+    $(".mainSlider").owlCarousel({
         items: 2,
         loop: true,
         margin: 48,
@@ -27,4 +27,79 @@ $(document).ready(function () {
         }
     })
 
+});
+
+
+$(document).ready(function () {
+    const nextIcon = '<img class="nextIcon" src="./assets/images/prewIcon.svg">'
+    const prewIcon = '<img class="prewIcon" src="./assets/images/nextIcon.svg">'
+    $(".tabSlider").owlCarousel({
+        items: 1,
+        loop: true,
+        margin: 48,
+        nav: true,
+        autoWidth: false,
+        dots: true,
+        autoplay: false,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        navText: [
+            nextIcon, prewIcon
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    })
+
+});
+
+
+
+
+$(document).ready(function () {
+    $('.flip-button1').click(function () {
+        $('.card1 ').addClass('flipped');
+    });
+
+    $('.flip-back-button1').click(function () {
+        $('.card1 ').removeClass('flipped');
+    });
+});
+
+$(document).ready(function () {
+    $('.flip-button2').click(function () {
+        $('.card2 ').addClass('flipped');
+    });
+
+    $('.flip-back-button2').click(function () {
+        $('.card2 ').removeClass('flipped');
+    });
+});
+
+$(document).ready(function () {
+    $('.flip-button3').click(function () {
+        $('.card3 ').addClass('flipped');
+    });
+
+    $('.flip-back-button3').click(function () {
+        $('.card3 ').removeClass('flipped');
+    });
+});
+
+$(document).ready(function () {
+    $('.flip-button4').click(function () {
+        $('.card4 ').addClass('flipped');
+    });
+
+    $('.flip-back-button4').click(function () {
+        $('.card4 ').removeClass('flipped');
+    });
 });
